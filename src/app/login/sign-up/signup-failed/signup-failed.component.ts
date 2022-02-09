046f0cf5af548d4205ee6227a39e0b9b1832a3c9
ANKIT <ankitkumar6220@gmail.com>
2022-02-09 23:09:17 +0530

    signup-failed component is created in login module

---
+++ src/app/login/sign-up/signup-failed/signup-failed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-failed',
+  templateUrl: './signup-failed.component.html',
+  styleUrls: ['./signup-failed.component.scss']
+})
+export class SignupFailedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

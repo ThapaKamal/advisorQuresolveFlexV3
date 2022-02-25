c69eeadedba60baa9849571a154444548b1c4eae
ANKIT <ankitkumar6220@gmail.com>
2022-02-25 20:47:19 +0530

    created RegistrationPayload

---
+++ src/app/legaladvisor/registration/Payload/registrationPayload.ts
@@ -0,0 +1,51 @@
+export class RegistrationPayload {
+    enteredName: String='';
+    enteredEmail: String='';
+    enteredPhone: String='';
+    enteredDoB: String='';
+    gender: String='';
+    selectedlanguage: String='';
+    uploadedPhoto: String='';
+
+    // accomplishments!: string[];AccomplishmentPayload
+    accomplishments: String='';
+    clients: String='';
+    selectedBaseCity: String='';
+    enteredenrollment: String='';
+    linkedinUrl: String='';
+    selectedAreaofpractice: String='';
+    selectedCourtOfPractice: String='';
+    selectedYearsOfExp: String='';
+    selectedBarMembership: String='';
+    barCouncilId: String='';
+
+    educations: String='';
+    // enteredDegreeName: String='';
+    // enteredCollegeName: String='';
+    // yearOfPassing: String='';
+    // selectedTypeofDegree: String='';
+
+    certs: String='';
+    // certList != certificationPayload[],
+    // enteredCertificateName: String='';
+    // enteredIssuingAuthority: String='';
+    // dateofIssuance: String='';
+  
+    enteredBeneficiaryName: String='';
+    enteredAccountNumber: String='';
+    enteredIfscCode: String='';
+    confirmAccountnumber: String='';
+    selectedBankName: String='';
+    
+    enteredAddressline1: String='';
+    enteredAddressline2: String='';
+    enteredPinCode: String='';
+    enteredCity: String='';
+    enteredPinCodeArea: String='';
+    enteredState: String='';
+    selectedTypeofAddress: String='';
+
+    terms: String='Yes';
+
+
+}

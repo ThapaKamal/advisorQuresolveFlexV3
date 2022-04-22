71f85a670580b89a1f67a16d31e1b781b354d0b6
ANKIT <ankitkumar6220@gmail.com>
2022-04-22 14:59:43 +0530

    updated profile service

--- src/app/modules/profile/advisorProfileservice/advisor-profile.service.ts
+++ src/app/modules/profile/advisorProfileservice/advisor-profile.service.ts
@@ -5,59 +5,131 @@ import { TypeOfAddressListService } from 'src/app/legaladvisor/legalService/type
   providedIn: 'root'
 })
 export class AdvisorProfileService {
+  [x: string]: any;
 
   constructor(
     // private typeOfAddressListService: TypeOfAddressListService,
-  )
-  {
+  ) {
 
   }
+  advisorProfileList() {
+    return [{
+      receivedName: "Ankit Kumar",
+      receivedEmail: 'ankitkumar6220@gmail.com',
+      receivedPhone: 8851421261,
+      receivedDoB: new Date('2020-09-28'),
+      receivedGender: { id: '1', value: "Male" },
+      receivedLanguage: { id: '1', value: "English" },
 
-  // enteredName:'Ankit Kumar',
-  // enteredEmail: 'ankitkumar6220@gmail.com',
-  // enteredPhone: '8851421261',
-  // enteredDoB: '06/02/2000',
-  // gender: '1',
-  // selectedlanguage: 'English',
+      receivedBaseCity: { id: 1, name: "Delhi" },
+      receivedEnrollment: 'XYZ123',
+      receivedLinkedinUrl: 'https://www.linkedin.com/in/ankit-kumar-2a6a501b3/',
+      // receivedAreaofpractice: 'Matrimonial Family Disputes, Civil Disputes, Marriage Registration	',
+      // receivedCourtOfPractice: 'Supreme Court, Delhi High court, Rajasthan HC',
+      receivedYearsOfExp: { id: 1, name: "1 Years" },
+      receivedBarMembership:{ id: 1, name: 'Bar council of Delhi'},
+      receivedBarCouncilId: 'IMG_20210825_195320.jpg',
 
-  // accomplishments: '1',
-  // clients: '1',
-  // selectedBaseCity: 'Delhi NCR',
-  // enteredenrollment: 'XYZ123',
-  // linkedinUrl: 'https://www.linkedin.com/in/ankit-kumar-2a6a501b3/',
-  // selectedAreaofpractice: 'Matrimonial Family Disputes, Civil Disputes, Marriage Registration	',
-  // selectedCourtOfPractice: 'Supreme Court, Delhi High court, Rajasthan HC',
-  // selectedYearsOfExp: '3 Years',
-  // selectedBarMembership: 'Bar council of Delhi',
-  // barCouncilId: 'IMG_20210825_195320.jpg',
+      accomplishments: [
 
-  // enteredDegreeName: 'LLB',
-  // enteredCollegeName: 'IILM',
-  // yearOfPassing: '2022',
-  // selectedTypeofDegree: "Bachelor's",
+        {
+          receivedHeading: "22222",
+          receivedDesc: "222"
+        },
+        {
+          receivedHeading: "3333",
+          receivedDesc: "33333"
+        },
+        {
+          receivedHeading: "6",
+          receivedDesc: "33366633"
+        },
+        {
+          receivedHeading: "4",
+          receivedDesc: "33366633"
+        }
+        ,
+        {
+          receivedHeading: "1",
+          receivedDesc: "33366633"
+        }
+      ],
 
 
-  // enteredCertificateName: 'xyz',
-  // enteredIssuingAuthority: 'xyz',
-  // dateofIssuance: '06/02/2021',
-  // education =
+      clients: [
+        {
+          "receivedMajorClient": "Ankit"
+        },
+        {
+          "receivedMajorClient": "asdsad"
+        }
+      ],
+
+
+      educations: [
+        {
+          receivedDegreeName: 'LLB',
+          receivedCollegeName: 'IILM',
+          receivedYearOfPassing: '2020',
+          receivedTypeofDegree: { id: 1, name: "Bachelor's" },
+          receivedPrimary: false
+        },
+        {
+          receivedDegreeName: 'LLB',
+          receivedCollegeName: 'IIM',
+          receivedYearOfPassing: '2022',
+          receivedTypeofDegree: { id: 2, name: "Master's" },
+          receivedPrimary: true
+        }
+      ],
+      certs: [
+        {
+          receivedCertificateName: 'xyz',
+          receivedIssuingAuthority: 'xyz',
+          receivedDateofIssuance: new Date('2020-09-28'),
+        }
+      ],
+
+      receivedBeneficiaryName:'Ankit kumar',
+      receivedIfscCode:'SBIN0008442',
+      receivedAccountNumber:1234567891,
+      receivedConfirmAccountnumber:1234567891,
+      receivedSelectedBankName:{ id: 2, name: "SBI" },
+    
+      receivedAddressline1: '3015,Sector A, Pocket B&C',
+      receivedAddressline2: 'Vasant Kunj',
+      receivedPinCode: '110070',
+      receivedCity: 'New Delhi',
+          receivedPinCodeArea: 'Vasant Kunj',
+          receivedState: 'Delhi',
+          receivedTypeofAddress:{ id: 1, name: "Office" } ,
+      
+    }]
+
+  }
+  // getAccomplishments = [
+
   //   {
-  //     enteredBeneficiaryName: 'Ankit kumar',
-  //     enteredAccountNumber: '1234567891',
-  //     enteredIfscCode: 'SBIN0122222',
-  //     confirmAccountnumber: '1234567891',
-  //     selectedBankName: 'STATE BANK OF INDIA',
-  //   }
-
-  // advisorProfile =
+  //     receivedHeading: "22222",
+  //     receivedDesc: "222"
+  //   },
   //   {
-  //     enteredAddressline1: '3015,Sector A, Pocket B&C',
-  //     enteredAddressline2: 'Vasant Kunj',
-  //     enteredPinCode: '110070',
-  //     enteredCity: 'New Delhi',
-  //     enteredPinCodeArea: 'Vasant Kunj',
-  //     enteredState: 'Delhi',
-  //     selectedTypeofAddress: this.typeOfAddressListService.typeOfAddressList,
+  //     receivedHeading: "3333",
+  //     receivedDesc: "33333"
+  //   },
+  //   {
+  //     receivedHeading: "6",
+  //     receivedDesc: "33366633"
+  //   },
+  //   {
+  //     receivedHeading: "4",
+  //     receivedDesc: "33366633"
   //   }
+  //   ,
+  //   {
+  //     receivedHeading: "1",
+  //     receivedDesc: "33366633"
+  //   }
+  // ]
 
 }
